a5d88ec7c553ce54218df16f067f5c82291ac8cf
Muhammad <mohamedkam000@gmail.com>
2025-08-04 14:03:26 +0200

    Colour enhancement

--- app/src/main/java/dev/muhammad/applock/features/appintro/ui/AppIntroScreen.kt
+++ app/src/main/java/dev/muhammad/applock/features/appintro/ui/AppIntroScreen.kt
@@ -184,7 +184,7 @@ fun AppIntroScreen(navController: NavController) {
             title = "Welcome to App Lock",
             description = "This is an app locker made for a university project.",
             icon = Icons.Filled.Lock,
-            backgroundColor = Color(0xFF0ADCFF),
+            backgroundColor = Color(0xFF00A5FF),
             contentColor = Color.White,
             onNext = { true }
         ),
