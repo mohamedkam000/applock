7f46eb702c1921e2f12e44c8e5b35f89be89d099
Muhammad <mohamedkam000@gmail.com>
2025-11-22 08:04:00 +0000

    re-construct gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,3 @@
-import org.jetbrains.kotlin.gradle.dsl.JvmTarget
-
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
@@ -12,10 +10,18 @@ android {
 
     defaultConfig {
         applicationId = "com.app.lock"
-        minSdk = 26
+        minSdk = 31
         targetSdk = 36
         versionCode = 1
         versionName = "1"
+        ndk {
+            abiFilters += listOf("arm64-v8a")
+            debugSymbolLevel 'none'
+        }
+
+        androidResources {
+            localeFilters += setOf("en")
+        }
     }
 
     signingConfigs {
@@ -36,17 +42,19 @@ android {
         }
     }
 
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
-
-    kotlin.compilerOptions {
-        jvmTarget.set(JvmTarget.JVM_11)
-    }
-
     buildFeatures {
         compose = true
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_25
+        targetCompatibility = JavaVersion.VERSION_25
+    }
+
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
     }
 
     dependenciesInfo {
@@ -57,16 +65,16 @@ android {
 
 dependencies {
     implementation(project(":appintro"))
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.lifecycle.runtime.ktx)
-    implementation(libs.androidx.activity.compose)
-    implementation(libs.androidx.activity.ktx)
-    implementation(libs.androidx.fragment.ktx)
-    implementation(platform(libs.androidx.compose.bom))
-    implementation(libs.androidx.ui.tooling.preview)
-    implementation(libs.androidx.material3)
-    implementation(libs.androidx.biometric)
-    implementation(libs.androidx.material.icons.core)
-    implementation(libs.androidx.material.icons.extended)
-    implementation(libs.androidx.navigation.compose)
+    implementation("androidx.core:core-ktx:1.17.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.10.0")
+    implementation("androidx.activity:activity-compose:1.12.0")
+    implementation("androidx.activity:activity-ktx:1.12.0")
+    implementation("androidx.fragment:fragment-ktx:1.8.2")
+    implementation(platform("androidx.compose:compose-bom:2025.11.00"))
+    implementation("androidx.compose.ui:ui-tooling-preview:1.9.5")
+    implementation("androidx.compose.material3:material3:1.5.0-alpha09")
+    implementation("androidx.biometric:biometric:1.2.0-alpha05")
+    implementation("androidx.compose.material:material-icons-core:1.9.5")
+    implementation("androidx.compose.material:material-icons-extended:1.9.5")
+    implementation("androidx.navigation:navigation-compose:2.8.0")
 }
